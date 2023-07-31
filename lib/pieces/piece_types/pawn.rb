033d018660944104eb4c4d175071bbe332854d48
Wes Hendley <wrhendley@gmail.com>
2023-07-31 16:43:54 -0500

    Add #set_squares_threatened

--- lib/pieces/piece_types/pawn.rb
+++ lib/pieces/piece_types/pawn.rb
@@ -1,7 +1,18 @@
-require 'piece'
+require_relative '../piece'
 
 class Pawn < Piece
   def initialize(x, y, color)
     super(x, y, color)
+    set_squares_threatened
+  end
+
+  def set_squares_threatened
+    if @color == 'black'
+      @squares_threatened << [@xPos - 1, @yPos + 1] unless (@xPos == 0 || @yPos == 7)
+      @squares_threatened << [xPos + 1, yPos + 1] unless (@xPos == 7 || @yPos == 7)
+    elsif @color == 'white'
+      @squares_threatened << [@xPos - 1, @yPos - 1] unless (@xPos == 0 || @yPos == 0)
+      @squares_threatened << [@xPos + 1, @yPos - 1] unless (@xPos == 7 || @yPos == 0)
+    end
   end
 end
