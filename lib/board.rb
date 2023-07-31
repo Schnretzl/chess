b300f5b984814c85631937fc2a6110ef52c2b337
Wes Hendley <wrhendley@gmail.com>
2023-07-31 13:25:33 -0500

    Add #add_black_pieces, #add_white_pieces, and #add_pawns

--- lib/board.rb
+++ lib/board.rb
@@ -10,5 +10,33 @@ class Board
     add_black_pieces
   end
 
+
+  private
+
+  def add_black_pieces
+    add_pawns(black)
+    add_rooks(black)
+    add_knights(black)
+    add_bishops(black)
+    add_queen(black)
+    add_king(black)
+  end
+  
+  def add_white_pieces
+    add_pawns(white)
+    add_rooks(white)
+    add_knights(white)
+    add_bishops(white)
+    add_queen(white)
+    add_king(white)
+  end
+
+  def add_pawns(color)
+    if color == 'black'
+      @grid[1].map!.with_index { |_square, index| Pawn.new(index, 1, 'black') }
+    elsif color == 'white'
+      @grid[6].map!.with_index { |_square, index| Pawn.new(index, 6, 'white') }
+    end
+  end
   
 end
