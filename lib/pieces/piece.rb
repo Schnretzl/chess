b43f7933ed15908f59729472a4190870ff41d92e
Wes Hendley <wrhendley@gmail.com>
2023-08-01 15:29:30 -0500

    Add board class variable and #set_board

--- lib/pieces/piece.rb
+++ lib/pieces/piece.rb
@@ -1,10 +1,16 @@
 class Piece
+  @@board = nil
+
   def initialize(x, y, color)
-    @xPos = x
-    @yPos = y
+    @x_pos = x
+    @y_pos = y
     @color = color
     @has_moved = false
     @squares_threatened = []
     @valid_moves = []
+  end
+
+  def self.set_board(board)
+    @@board = board
   end
 end
