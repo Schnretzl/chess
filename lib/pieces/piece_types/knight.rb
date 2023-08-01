66d67ba4924cb022a8b9d67d06896b3339109532
Wes Hendley <wrhendley@gmail.com>
2023-08-01 15:54:20 -0500

    Add #set_squares_threatened and #set_valid_moves

--- lib/pieces/piece_types/knight.rb
+++ lib/pieces/piece_types/knight.rb
@@ -1,7 +1,26 @@
-require 'piece'
+require_relative '../piece'
 
 class Knight < Piece
   def initialize(x, y, color)
     super(x, y, color)
+    set_squares_threatened
+    set_valid_moves
+  end
+
+  def set_squares_threatened
+    @squares_threatened << [@x_pos + 1, @y_pos + 2] unless (@x_pos >= 7 || @y_pos >= 6)
+    @squares_threatened << [@x_pos - 1, @y_pos + 2] unless (@x_pos == 0 || @y_pos >= 6)
+    @squares_threatened << [@x_pos - 2, @y_pos + 1] unless (@x_pos <= 1 || @y_pos >= 7)
+    @squares_threatened << [@x_pos - 2, @y_pos - 1] unless (@x_pos <= 1 || @y_pos == 0)
+    @squares_threatened << [@x_pos - 1, @y_pos - 2] unless (@x_pos == 0 || @y_pos <= 1)
+    @squares_threatened << [@x_pos + 1, @y_pos - 2] unless (@x_pos >= 7 || @y_pos <= 1)
+    @squares_threatened << [@x_pos + 2, @y_pos - 1] unless (@x_pos >= 6 || @y_pos == 0)
+    @squares_threatened << [@x_pos + 2, @y_pos + 1] unless (@x_pos >= 6 || @y_pos >= 7)
+  end
+
+  def set_valid_moves
+    @squares_threatened.each do |move|
+      @valid_moves << move if @@board.grid[move[0]][move[1]] == ' '
+    end
   end
 end
