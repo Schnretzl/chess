a40cccac08a315c5ada830f9b80be7e118801f68
Wes Hendley <wrhendley@gmail.com>
2023-08-01 15:26:32 -0500

    Add #set_valid_moves

--- lib/pieces/piece_types/pawn.rb
+++ lib/pieces/piece_types/pawn.rb
@@ -4,15 +4,23 @@ class Pawn < Piece
   def initialize(x, y, color)
     super(x, y, color)
     set_squares_threatened
+    set_valid_moves
   end
 
   def set_squares_threatened
     if @color == 'black'
-      @squares_threatened << [@xPos - 1, @yPos + 1] unless (@xPos == 0 || @yPos == 7)
-      @squares_threatened << [xPos + 1, yPos + 1] unless (@xPos == 7 || @yPos == 7)
+      @squares_threatened << [@x_pos - 1, @y_pos + 1] unless (@x_pos == 0 || @y_pos == 7)
+      @squares_threatened << [@x_pos + 1, @y_pos + 1] unless (@x_pos == 7 || @y_pos == 7)
     elsif @color == 'white'
-      @squares_threatened << [@xPos - 1, @yPos - 1] unless (@xPos == 0 || @yPos == 0)
-      @squares_threatened << [@xPos + 1, @yPos - 1] unless (@xPos == 7 || @yPos == 0)
+      @squares_threatened << [@x_pos - 1, @y_pos - 1] unless (@x_pos == 0 || @y_pos == 0)
+      @squares_threatened << [@x_pos + 1, @y_pos - 1] unless (@x_pos == 7 || @y_pos == 0)
+    end
+  end
+
+  def set_valid_moves
+    if @color == 'black'
+      @y_pos < 7 && @@board.grid[@x_pos][@y_pos + 1] == ' ' && (@valid_moves << [@x_pos, @y_pos + 1])
+      !@has_moved && (@valid_moves << [@x_pos, @y_pos + 2])
     end
   end
 end
