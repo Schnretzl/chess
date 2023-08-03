bcb0e019a57bcef811dd86dbbb7cd4aa1062260f
Wes Hendley <wrhendley@gmail.com>
2023-08-03 16:14:30 -0500

    Finish #add_squares_up, add #add_squares_down, #add_squares_left, and #add_squares_right

--- lib/pieces/piece_types/rook.rb
+++ lib/pieces/piece_types/rook.rb
@@ -10,17 +10,44 @@ class Rook < Piece
   def set_squares_threatened
     @squares_threatened = []
     add_squares_up
-    # add_squares_down
-    # add_squares_left
-    # add_squares_right
+    add_squares_down
+    add_squares_left
+    add_squares_right
   end
 
   def add_squares_up
     column = @x_pos
+    row = @y_pos - 1
+    until !@@board.empty?(row, column) || row < 0
+      @squares_threatened << [row, column]
+      row -= 1
+    end
+  end
+
+  def add_squares_down
+    column = @x_pos
+    row = @y_pos + 1
+    until !@@board.empty?(row, column) || row > 7
+      @squares_threatened << [row, column]
+      row += 1
+    end
+  end
+
+  def add_squares_left
+    column = @x_pos - 1
     row = @y_pos
-    until !@@board.empty?(row, column) || row <= 0
-      @squares_threatened << [column, row]
+    until !@@board.empty?(row, column) || column < 0
+      @squares_threatened << [row, column]
       column -= 1
+    end
+  end
+
+  def add_squares_right
+    column = @x_pos + 1
+    row = @y_pos
+    until !@@board.empty?(row, column) || column > 7
+      @squares_threatened << [row, column]
+      column += 1
     end
   end
 end
