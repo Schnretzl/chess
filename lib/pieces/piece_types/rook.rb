7d14133b1b417ec601cd0da9613a740282bd5232
Wes Hendley <wrhendley@gmail.com>
2023-08-03 11:06:23 -0500

    Add #set_squares_threatened and #add_squares_up

--- lib/pieces/piece_types/rook.rb
+++ lib/pieces/piece_types/rook.rb
@@ -3,5 +3,24 @@ require_relative '../piece'
 class Rook < Piece
   def initialize(y, x, color)
     super(y, x, color)
+    set_squares_threatened
+    # set_valid_moves
+  end
+
+  def set_squares_threatened
+    @squares_threatened = []
+    add_squares_up
+    # add_squares_down
+    # add_squares_left
+    # add_squares_right
+  end
+
+  def add_squares_up
+    column = @x_pos
+    row = @y_pos
+    until !@@board.empty?(row, column) || row <= 0
+      @squares_threatened << [column, row]
+      column -= 1
+    end
   end
 end
