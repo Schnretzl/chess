2f3ea177795f6ad90936e7264c130ab1a628aa26
Wes Hendley <wrhendley@gmail.com>
2023-08-03 16:15:28 -0500

    Add tests for #set_squares_threatened

--- spec/pieces/piece_types/rook_spec.rb
+++ spec/pieces/piece_types/rook_spec.rb
@@ -1,17 +1,25 @@
 require './lib/pieces/piece_types/rook'
+require './lib/board'
 
 describe Rook do
-  subject(:black_rook) { described_class.new(5, 5, 'black') }
-  let(:board) { instance_double('Board', grid: Array.new(8) { Array.new(8, ' ') }) }
+  subject(:black_rook) { described_class.new(4, 5, 'black') }
+  let(:board) { Board.new }
 
   before do
     Rook.set_board(board)
+    allow(board).to receive(:empty?).and_call_original
   end
 
   describe '#set_squares_threatened' do
     it 'sets threatened squares for a black rook' do
+      board.grid[0][5] = ' '
+      board.grid[1][5] = ' '
       black_rook.set_squares_threatened
-      expect(black_rook.instance_variable_get(:@squares_threatened)).to contain_exactly([4, 5], [3, 5], [2, 5], [1, 5], [0, 5])
+      expect(black_rook.instance_variable_get(:@squares_threatened)).to \
+        contain_exactly([3, 5], [2, 5], [1, 5], [0, 5], \
+                        [5, 5], \
+                        [4, 4], [4, 3], [4, 2], [4, 1], [4, 0], \
+                        [4, 6], [4, 7])
     end
   end
 end
