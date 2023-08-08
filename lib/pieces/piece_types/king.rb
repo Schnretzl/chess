ac03921daed68a3688874ea891e7a95107c449a6
Wes Hendley <wrhendley@gmail.com>
2023-08-08 11:15:53 -0500

    Add @set_squares_threatened

--- lib/pieces/piece_types/king.rb
+++ lib/pieces/piece_types/king.rb
@@ -3,5 +3,20 @@ require_relative '../piece'
 class King < Piece
   def initialize(y, x, color, board)
     super(y, x, color)
+    set_squares_threatened(board)
+  end
+
+  def set_squares_threatened(board)
+    @set_squares_threatened << [@y_pos - 1, @x_pos] unless @y_pos.zero? || board.grid[@y_pos - 1, @x_pos] != ' '
+    @set_squares_threatened << [@y_pos - 1, @x_pos + 1] unless @y_pos.zero? || @x_pos == 7 ||
+                                                               board.grid[@y_pos - 1, @x_pos + 1] != ' '
+    @set_squares_threatened << [@y_pos, @x_pos + 1] unless @x_pos == 7 || board.grid[@y_pos, @x_pos + 1] != ' '
+    @set_squares_threatened << [@y_pos + 1, @x_pos + 1] unless @y_pos == 7 || @x_pos == 7 ||
+                                                               board.grid[@y_pos + 1, @x_pos + 1] != ' '
+    @set_squares_threatened << [@y_pos + 1, @x_pos] unless @y_pos == 7 || board.grid[@y_pos + 1, @x_pos] != ' '
+    @set_squares_threatened << [@y_pos + 1, @x_pos - 1] unless @y_pos == 7 || @x_pos.zero? ||
+                                                               board.grid[@y_pos + 1, @x_pos - 1] != ' '
+    @set_squares_threatened << [@y_pos, @x_pos - 1] unless @x_pos == 0 || board.grid[@y_pos, @x_pos - 1] != ' '
+    @set_squares_threatened << [@y_pos + 1, @x_pos - 1] unless @y_pos == 0
   end
 end
