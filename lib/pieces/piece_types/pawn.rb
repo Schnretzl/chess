2c8fcffb05fc790410a4291c4845c768fd38bdb9
Wes Hendley <wrhendley@gmail.com>
2023-08-08 09:38:58 -0500

    Make #set_squares_threatened only add blank squares

--- lib/pieces/piece_types/pawn.rb
+++ lib/pieces/piece_types/pawn.rb
@@ -7,15 +7,19 @@ class Pawn < Piece
     set_valid_moves(board)
   end
 
-  def set_squares_threatened
+  def set_squares_threatened(board)
     if @color == 'black'
       @squares_threatened = []
-      @squares_threatened << [@y_pos + 1, @x_pos - 1] unless (@y_pos == 7 || @x_pos == 0)
-      @squares_threatened << [@y_pos + 1, @x_pos + 1] unless (@y_pos == 7 || @x_pos == 7)
+      @squares_threatened << [@y_pos + 1, @x_pos - 1] unless @y_pos == 7 || @x_pos.zero? ||
+                                                             board[@y_pos + 1, @x_pos - 1] != ' '
+      @squares_threatened << [@y_pos + 1, @x_pos + 1] unless @y_pos == 7 || @x_pos == 7 ||
+                                                             board[@y_pos + 1, @x_pos + 1] != ' '
     elsif @color == 'white'
       @squares_threatened = []
-      @squares_threatened << [@y_pos - 1, @x_pos - 1] unless (@y_pos == 0 || @x_pos == 0)
-      @squares_threatened << [@y_pos - 1, @x_pos + 1] unless (@y_pos == 0 || @x_pos == 7)
+      @squares_threatened << [@y_pos - 1, @x_pos - 1] unless @y_pos == 0 || @x_pos == 0 ||
+                                                             board.grid[@y_pos - 1][@x_pos - 1]
+      @squares_threatened << [@y_pos - 1, @x_pos + 1] unless @y_pos == 0 || @x_pos == 7 ||
+                                                             board.grid[@y_pos - 1, @x_pos + 1]
     end
   end
 
