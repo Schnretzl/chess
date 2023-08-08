d7df0a8504b037040b9a23d6a160c453b35fdeea
Wes Hendley <wrhendley@gmail.com>
2023-08-08 09:40:32 -0500

    Make #set_squares_threatened only add blank squares

--- lib/pieces/piece_types/knight.rb
+++ lib/pieces/piece_types/knight.rb
@@ -3,19 +3,20 @@ require_relative '../piece'
 class Knight < Piece
   def initialize(y, x, color, board)
     super(y, x, color)
-    set_squares_threatened
+    set_squares_threatened(board)
     set_valid_moves(board)
   end
 
   def set_squares_threatened
-    @squares_threatened << [@y_pos + 1, @x_pos + 2] unless (@y_pos >= 7 || @x_pos >= 6)
-    @squares_threatened << [@y_pos - 1, @x_pos + 2] unless (@y_pos == 0 || @x_pos >= 6)
-    @squares_threatened << [@y_pos - 2, @x_pos + 1] unless (@y_pos <= 1 || @x_pos >= 7)
-    @squares_threatened << [@y_pos - 2, @x_pos - 1] unless (@y_pos <= 1 || @x_pos == 0)
-    @squares_threatened << [@y_pos - 1, @x_pos - 2] unless (@y_pos == 0 || @x_pos <= 1)
-    @squares_threatened << [@y_pos + 1, @x_pos - 2] unless (@y_pos >= 7 || @x_pos <= 1)
-    @squares_threatened << [@y_pos + 2, @x_pos - 1] unless (@y_pos >= 6 || @x_pos == 0)
-    @squares_threatened << [@y_pos + 2, @x_pos + 1] unless (@y_pos >= 6 || @x_pos >= 7)
+    @squares_threatened << [@y_pos + 1, @x_pos + 2] unless @y_pos >= 7 || @x_pos >= 6 ||
+                                                           board.grid[@y_pos + 1, @x_pos + 2] != ' '
+    @squares_threatened << [@y_pos - 1, @x_pos + 2] unless @y_pos.zero? || @x_pos >= 6
+    @squares_threatened << [@y_pos - 2, @x_pos + 1] unless @y_pos <= 1 || @x_pos >= 7
+    @squares_threatened << [@y_pos - 2, @x_pos - 1] unless @y_pos <= 1 || @x_pos.zero?
+    @squares_threatened << [@y_pos - 1, @x_pos - 2] unless @y_pos.zero? || @x_pos <= 1
+    @squares_threatened << [@y_pos + 1, @x_pos - 2] unless @y_pos >= 7 || @x_pos <= 1
+    @squares_threatened << [@y_pos + 2, @x_pos - 1] unless @y_pos >= 6 || @x_pos.zero?
+    @squares_threatened << [@y_pos + 2, @x_pos + 1] unless @y_pos >= 6 || @x_pos >= 7
   end
 
   def set_valid_moves(board)
