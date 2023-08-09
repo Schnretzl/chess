8b4ad8eb267667e0682388a7821e64261eee820c
Wes Hendley <wrhendley@gmail.com>
2023-08-09 15:47:20 -0500

    Use #negative? instead of < 0

--- lib/pieces/piece_movement/threatens_orthogonally.rb
+++ lib/pieces/piece_movement/threatens_orthogonally.rb
@@ -13,7 +13,7 @@ module ThreatensOrthogonally
   def add_squares_up(board)
     column = @x_pos
     row = @y_pos - 1
-    until !board.empty?(row, column) || row < 0
+    until !board.empty?(row, column) || row.negative?
       @squares_threatened << [row, column]
       row -= 1
     end
@@ -31,7 +31,7 @@ module ThreatensOrthogonally
   def add_squares_left(board)
     column = @x_pos - 1
     row = @y_pos
-    until !board.empty?(row, column) || column < 0
+    until !board.empty?(row, column) || column.negative?
       @squares_threatened << [row, column]
       column -= 1
     end
