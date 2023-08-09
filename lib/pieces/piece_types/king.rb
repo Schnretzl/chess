a4d0c18b321f3b389a7fd61687f43fe3ec01fa13
Wes Hendley <wrhendley@gmail.com>
2023-08-09 15:49:51 -0500

    Fix array referencing in #set_squares_threatened.  Add #set_valid_moves

--- lib/pieces/piece_types/king.rb
+++ lib/pieces/piece_types/king.rb
@@ -4,19 +4,31 @@ class King < Piece
   def initialize(y, x, color, board)
     super(y, x, color)
     set_squares_threatened(board)
+    set_valid_moves(board)
+    @is_in_check = false
   end
 
   def set_squares_threatened(board)
-    @set_squares_threatened << [@y_pos - 1, @x_pos] unless @y_pos.zero? || board.grid[@y_pos - 1, @x_pos] != ' '
-    @set_squares_threatened << [@y_pos - 1, @x_pos + 1] unless @y_pos.zero? || @x_pos == 7 ||
-                                                               board.grid[@y_pos - 1, @x_pos + 1] != ' '
-    @set_squares_threatened << [@y_pos, @x_pos + 1] unless @x_pos == 7 || board.grid[@y_pos, @x_pos + 1] != ' '
-    @set_squares_threatened << [@y_pos + 1, @x_pos + 1] unless @y_pos == 7 || @x_pos == 7 ||
-                                                               board.grid[@y_pos + 1, @x_pos + 1] != ' '
-    @set_squares_threatened << [@y_pos + 1, @x_pos] unless @y_pos == 7 || board.grid[@y_pos + 1, @x_pos] != ' '
-    @set_squares_threatened << [@y_pos + 1, @x_pos - 1] unless @y_pos == 7 || @x_pos.zero? ||
-                                                               board.grid[@y_pos + 1, @x_pos - 1] != ' '
-    @set_squares_threatened << [@y_pos, @x_pos - 1] unless @x_pos == 0 || board.grid[@y_pos, @x_pos - 1] != ' '
-    @set_squares_threatened << [@y_pos + 1, @x_pos - 1] unless @y_pos == 0
+    @squares_threatened << [@y_pos - 1, @x_pos] unless @y_pos.zero? || board.grid[@y_pos - 1][@x_pos] != ' '
+    @squares_threatened << [@y_pos - 1, @x_pos + 1] unless @y_pos.zero? || @x_pos == 7 ||
+                                                           board.grid[@y_pos - 1][@x_pos + 1] != ' '
+    @squares_threatened << [@y_pos, @x_pos + 1] unless @x_pos == 7 || board.grid[@y_pos][@x_pos + 1] != ' '
+    @squares_threatened << [@y_pos + 1, @x_pos + 1] unless @y_pos == 7 || @x_pos == 7 ||
+                                                           board.grid[@y_pos + 1][@x_pos + 1] != ' '
+    @squares_threatened << [@y_pos + 1, @x_pos] unless @y_pos == 7 || board.grid[@y_pos + 1][@x_pos] != ' '
+    @squares_threatened << [@y_pos + 1, @x_pos - 1] unless @y_pos == 7 || @x_pos.zero? ||
+                                                           board.grid[@y_pos + 1][@x_pos - 1] != ' '
+    @squares_threatened << [@y_pos, @x_pos - 1] unless @x_pos.zero? || board.grid[@y_pos][@x_pos - 1] != ' '
+    @squares_threatened << [@y_pos - 1, @x_pos - 1] unless @y_pos.zero? || @x_pos.zero? ||
+                                                           board.grid[@y_pos - 1][@x_pos - 1] != ' '
+  end
+
+  def set_valid_moves(board)
+    @valid_moves = []
+    if @color == 'black'
+      @squares_threatened.each do |square|
+        @valid_moves << square unless board.black_king_threatened_squares.include?(square)
+      end
+    end
   end
 end
