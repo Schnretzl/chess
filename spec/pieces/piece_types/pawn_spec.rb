580c55b994012b782e14771be5b0f9dc744af636
Wes Hendley <wrhendley@gmail.com>
2023-08-09 15:59:32 -0500

    Refactor for class definition changes.  Add test for #set_valid_captures

--- spec/pieces/piece_types/pawn_spec.rb
+++ spec/pieces/piece_types/pawn_spec.rb
@@ -1,29 +1,33 @@
 require './lib/pieces/piece_types/pawn'
 
 describe Pawn do
-  subject(:black_pawn) { described_class.new(1, 1, 'black') }
-  subject(:white_pawn) { described_class.new(6, 2, 'white') }
   let(:board) { instance_double('Board', grid: Array.new(8) { Array.new(8, ' ') }) }
-
-  before do
-    Pawn.set_board(board)
-  end
+  subject(:black_pawn) { described_class.new(1, 1, 'black', board) }
+  subject(:white_pawn) { described_class.new(6, 2, 'white', board) }
 
   describe '#set_squares_threatened' do
     it 'sets threatened squares for a black pawn' do
-      expect(black_pawn.set_squares_threatened).to contain_exactly([0, 2], [2, 2])
+      expect(black_pawn.set_squares_threatened(board)).to contain_exactly([2, 0], [2, 2])
     end
   end
 
   describe '#set_valid_moves' do
     it 'sets valid moves for a black pawn' do
-      expect(black_pawn.set_valid_moves).to contain_exactly([2, 1], [3, 1])
+      expect(black_pawn.set_valid_moves(board)).to contain_exactly([2, 1], [3, 1])
     end
 
     it 'sets valid moves for a white pawn which has moved' do
       white_pawn.instance_variable_set(:@has_moved, true)
-      white_pawn.set_valid_moves
+      white_pawn.set_valid_moves(board)
       expect(white_pawn.instance_variable_get(:@valid_moves)).to contain_exactly([5, 2])
+    end
+
+    describe '#set_valid_captures' do
+      it 'sets valid captures for a black pawn' do
+        described_class.new(2, 0, 'white', board)
+        black_pawn.set_valid_captures(board)
+        expect(black_pawn.instance_variable_get(:@valid_captures)).to contain_exactly([2, 0])
+      end
     end
 
   end
