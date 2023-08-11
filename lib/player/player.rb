22a5fe4b5a3d4211378cf24040fd423f8f37dbf1
Wes Hendley <wrhendley@gmail.com>
2023-08-11 11:05:33 -0500

    Add @name and #prompt_for_name

--- lib/player/player.rb
+++ lib/player/player.rb
@@ -1,5 +1,17 @@
 class Player
   def initialize(color)
     @color = color
+    @name = prompt_for_name(color)
+  end
+
+  def prompt_for_name(color)
+    player_number = color == 'white' ? 1 : 2
+    loop do
+      puts "Enter player #{player_number} name:"
+      name = STDIN.gets.chomp
+      return name if name =~ /\A[\p{Letter}\p{Mark}]+\z/
+
+      puts 'Invalid name entered, please enter only letters.'
+    end
   end
 end
