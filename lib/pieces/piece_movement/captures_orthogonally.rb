173fbb811ace9f063c70f78a7153789c3471073e
Wes Hendley <wrhendley@gmail.com>
2023-08-16 12:58:29 -0500

    Add checks for index in range and is not empty before pushing capture to @valid_captures

--- lib/pieces/piece_movement/captures_orthogonally.rb
+++ lib/pieces/piece_movement/captures_orthogonally.rb
@@ -12,25 +12,29 @@ module CapturesOrthogonally
 
   def add_capture_up(board, opponent_color)
     row = @y_pos - 1
-    row -= 1 until row.negative? || !board.empty?(row, @x_pos)    
-    board.grid[row][@x_pos].color == opponent_color && (@valid_captures << [row, @x_pos])
+    row -= 1 until row.negative? || !board.empty?(row, @x_pos)
+    !row.negative? && !board.empty?(row, @x_pos) && board.grid[row][@x_pos].color == opponent_color &&
+      (@valid_captures << [row, @x_pos])
   end
 
   def add_capture_down(board, opponent_color)
     row = @y_pos + 1
     row += 1 until row > 7 || !board.empty?(row, @x_pos)
-    board.grid[row][@x_pos].color == opponent_color && (@valid_captures << [row, @x_pos])
+    row <= 7 && !board.empty?(row, @x_pos) && board.grid[row][@x_pos].color == opponent_color &&
+      (@valid_captures << [row, @x_pos])
   end
 
   def add_capture_left(board, opponent_color)
     column = @x_pos - 1
     column -= 1 until column.negative? || !board.empty?(@y_pos, column)
-    board.grid[@y_pos][column].color == opponent_color && (@valid_captures << [@y_pos, column])
+    !column.negative? && board.empty?(@y_pos, column) && board.grid[@y_pos][column].color == opponent_color &&
+      (@valid_captures << [@y_pos, column])
   end
 
   def add_capture_right(board, opponent_color)
     column = @x_pos + 1
     column += 1 until column > 7 || !board.empty?(@y_pos, column)
-    board.grid[@y_pos][column].color == opponent_color && (@valid_captures << [@y_pos, column])
+    column <= 7 && board.empty?(@y_pos, column) && board.grid[@y_pos][column].color == opponent_color &&
+      (@valid_captures << [@y_pos, column])
   end
 end
