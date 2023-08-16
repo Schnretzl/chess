2d9120b3bfd13e65d2bcabcc5b906794abbd7e04
Wes Hendley <wrhendley@gmail.com>
2023-08-16 14:01:23 -0500

    Add CapturesDiagonally module and use of #set_valid_captures in #initialize.  Use Array#clear

--- lib/pieces/piece_types/bishop.rb
+++ lib/pieces/piece_types/bishop.rb
@@ -1,16 +1,19 @@
 require_relative '../piece'
 require_relative '../piece_movement/threatens_diagonally'
+require_relative '../piece_movement/captures_diagonally'
 
 class Bishop < Piece
   include ThreatensDiagonally
+  include CapturesDiagonally
   def initialize(y, x, color, board)
     super(y, x, color)
     set_squares_threatened(board)
     set_valid_moves(board)
+    set_valid_captures(board)
   end
 
   def set_valid_moves(board)
-    @valid_moves = []
+    @valid_moves.clear
     @squares_threatened.each { |square| @valid_moves << square }
   end
 end
