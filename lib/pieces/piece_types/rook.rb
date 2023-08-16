bc38c4debacfacfe8f32b242995dd14fa8baac2f
Wes Hendley <wrhendley@gmail.com>
2023-08-16 12:55:20 -0500

    Use Array#clear

--- lib/pieces/piece_types/rook.rb
+++ lib/pieces/piece_types/rook.rb
@@ -1,16 +1,19 @@
 require_relative '../piece'
 require_relative '../piece_movement/threatens_orthogonally'
+require_relative '../piece_movement/captures_orthogonally'
 
 class Rook < Piece
   include ThreatensOrthogonally
+  include CapturesOrthogonally
   def initialize(y, x, color, board)
     super(y, x, color)
     set_squares_threatened(board)
     set_valid_moves(board)
+    set_valid_captures(board)
   end
 
   def set_valid_moves(board)
-    @valid_moves = []
+    @valid_moves.clear
     @squares_threatened.each { |square| @valid_moves << square }
   end
 end
