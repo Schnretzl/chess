ada181086ce29c629cce8808ca6d4116681f6250
Wes Hendley <wrhendley@gmail.com>
2023-08-17 10:17:46 -0500

    Add CapturesOrthogonally and CapturesDiagonally.  Add set_valid_captures

--- lib/pieces/piece_types/queen.rb
+++ lib/pieces/piece_types/queen.rb
@@ -1,15 +1,21 @@
 require_relative '../piece'
 require_relative '../piece_movement/threatens_orthogonally'
 require_relative '../piece_movement/threatens_diagonally'
+require_relative '../piece_movement/captures_orthogonally'
+require_relative '../piece_movement/captures_diagonally'
 
 class Queen < Piece
   include ThreatensOrthogonally
   include ThreatensDiagonally
+  include CapturesOrthogonally
+  include CapturesDiagonally
   def initialize(y, x, color, board)
     super(y, x, color)
     ThreatensOrthogonally.instance_method(:set_squares_threatened).bind(self).call(board)
     ThreatensDiagonally.instance_method(:set_squares_threatened).bind(self).call(board)
     set_valid_moves(board)
+    CapturesOrthogonally.instance_method(:set_valid_captures).bind(self).call(board)
+    CapturesDiagonally.instance_method(:set_valid_captures).bind(self).call(board)
   end
 
   def set_valid_moves(board)
